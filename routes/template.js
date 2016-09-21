c537e23385ef2348e5f9fe4bc20b9c7727e09125
digachandra <imdigachandra@gmail.com>
2016-09-21 21:30:05 +0700

    Add 'view files' for template

--- routes/template.js
+++ routes/template.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('template-list', { title: 'Cooking Fox' });
 });
 
 module.exports = router;
